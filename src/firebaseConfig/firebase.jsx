65d1bf126570600ba1aa2f9c992e1ebdc25fe467
Roberto_94 <robertoverdeciasanchez@gmail.com>
2023-12-29 22:03:11 -0500

    Configuracion de firebase

--- src/firebaseConfig/firebase.jsx
+++ src/firebaseConfig/firebase.jsx
@@ -1,21 +1,3 @@
-
-/* import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-
-
-const firebaseConfig = {
-  apiKey: "AIzaSyB-YaI1sLJSeFnSfq57bIE-flhBOMKIcnM",
-  authDomain: "web-site-idec.firebaseapp.com",
-  projectId: "web-site-idec",
-  storageBucket: "web-site-idec.appspot.com",
-  messagingSenderId: "1001994316155",
-  appId: "1:1001994316155:web:9e548ac83b621c069035de"
-};
-
-const app = initializeApp(firebaseConfig);
-
-export const db = getFirestore(app); */
-
 // Import the functions you need from the SDKs you need
 import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
